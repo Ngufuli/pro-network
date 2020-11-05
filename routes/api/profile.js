75122dcea31d19c97c55a6967e85489875a64280
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-11-06 00:20:04 +0530

    fixed bugs

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,16 +1,16 @@
 const express = require('express');
+const axios = require('axios');
 const config = require('config');
-const request = require('request');
-const normalize = require('normalize-url')
+const router = express.Router();
 const auth = require('../../middleware/auth');
-const {check, validationResult} = require('express-validator')
-
-const router  = express.Router();
-
+const { check, validationResult } = require('express-validator');
+// bring in normalize to give us a proper url, regardless of what user entered
+const normalize = require('normalize-url');
 const checkObjectId = require('../../middleware/checkObjectId');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 //Route         GET api/profile/me
 //Description   Get current user's profile
