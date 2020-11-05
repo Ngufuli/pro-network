f53bdb966c0ed3c64c215e1db5d34c4dfca7fc13
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-11-05 15:18:14 +0530

    user register api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const {check, validationResult} = require('express-validator')
 
 const router  = express.Router();
 
-//Route         GET api/users
-//Description   TEST ROUTE
+//Route         POST api/users
+//Description   Register user
 //Access        PUBLIC
-router.get('/', (req, res) => res.send(`Here is the users page`));
+router.post('/',[
+    check('name', 'Name field is required').not().isEmpty(),
+    check('email','Enter a valid email address').isEmail(),
+    check('password','Password field should have 6 or more characters').isLength({min: 6})
+], (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+    }
+
+    console.log(req.body);
+    res.send(`Here is the users page`)
+});
 
 module.exports = router;
