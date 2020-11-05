c9f2f14105dfca4a3e04fe4506a800f3031a7d26
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-11-05 17:26:04 +0530

    auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
 
 const router  = express.Router();
+
+const User = require('../../models/User');
 
 //Route         GET api/auth
 //Description   TEST ROUTE
 //Access        PUBLIC
-router.get('/', (req, res) => res.send(`Here is the auth page`));
+router.get('/',auth, async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        console.error(err);
+        res.status(500).send('Server error')
+    }
+});
 
 module.exports = router;
