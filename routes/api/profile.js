42095b99a1d719ce2798bbfa1d3f08d38b424b28
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-11-06 00:00:43 +0530

    github api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const config = require('config');
+const request = require('request');
 const normalize = require('normalize-url')
 const auth = require('../../middleware/auth');
 const {check, validationResult} = require('express-validator')
@@ -310,6 +311,28 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     } catch (error) {
       console.error(error);
       return res.status(500).json({ msg: 'Server error' });
+    }
+  });
+
+
+    // @route    GET api/profile/github/:username
+    // @desc     Get user repos from Github
+    // @access   Public
+router.get('/github/:username', async (req, res) => {
+    try {
+      const uri = encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      );
+      const headers = {
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubSecretKey')}`
+      };
+  
+      const gitHubResponse = await axios.get(uri, { headers });
+      return res.json(gitHubResponse.data);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(404).json({ msg: 'No Github profile found' });
     }
   });
   
